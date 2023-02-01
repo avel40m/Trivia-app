7b6a7fac16761599aecb91795c488849741400fd
avel40m <avel40m@gmail.com>
2023-01-31 22:55:47 -0300

    update question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,39 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { gameLink } from "../function/gameLink";
+import '../style/loader.css';
 
 const Question = () => {
     const [array, setArray] = useState([]);
+    const [usuario,setUsuario] = useState([]);
 
-    const getData = async () => {
-        let ques = [];
-        ques = await await JSON.parse(localStorage.getItem('question'));
-        console.log(ques);
-        const res = await gameLink(ques);
-        console.log(res[0].category);
-        setArray(res);
-    }
-    
-    useEffect( () => {
-        getData(); 
-    },[])
+        useEffect(() => {
+          let ques = JSON.parse(localStorage.getItem('question'));
+          setUsuario(JSON.parse(localStorage.getItem('usuario')));
 
-
+            async function fetchData(){
+              const res = await gameLink(ques);
+              setArray(res);
+            }
+            fetchData();
+        }, []);
+// console.log(array);
+     if (array.length === 0) {
+      return <div className="lds-dual-ring margin-loader"></div>
+     }
 
     return (
     <div className="container">
       <div className="card">
         <div className="card-header">
-          <h1>Question: 1</h1>
+          <h1>Question: {array.length}</h1>
         </div>
         <div className="card-container">
           <div className="card-col">
-            <h1 className="title">Avelino Hector Mendez</h1>
-            <p className="card-button m20 bc-orange">Categoria: history</p>
-            <p className="card-button m20 bc-orange">Dificultad: Easy</p>
+            <h1 className="title">{usuario.username}</h1>
+            <p className="card-button m20 bc-orange">Categoria: {array[0].category}</p>
+            <p className="card-button m20 bc-orange">Dificultad: {array[0].difficulty}</p>
             <p className="card-button m20 bc-orange">Tiempo: 10:02:17</p>
           </div>
           <div className="card-col p-question">
-            <p className="title-question">Which film won the Academy Award for Best Picture in 1962?"</p>
+            <p className="title-question">{array[0].question}</p>
             <form>
                 <div className="form-group pt10" >
                     <select name="" id="" className="form-input">
